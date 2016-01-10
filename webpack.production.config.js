28c708f55c199317c89b502f03b058106bbed3c5
Christine Cha <hello@christinecha.com>
2016-01-10 00:36:06 -0500

    Updated bundle for production.

---
+++ webpack.production.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        loaders: ['babel-loader?presets[]=react,presets[]=es2015'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel-loader?presets[]=react,presets[]=es2015'],
+        exclude: /node_modules/
+      },
+      { test: /\.css$/, loader: "style!css" }
+    ],
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ]
+};
